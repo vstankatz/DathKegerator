4ffb2a82746015bdd38e35055560b86d64bbfaa2
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-14 15:02:44 -0800

    Display keg list on front.

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import './Body.css';
+import Keg from './Keg';
+
+var masterKegList = [
+  {
+    name: 'Obi Wan Porter',
+    content: 4.5,
+    size: 124,
+    price: 4
+  },
+  {
+    name: 'Princess Lei Ale',
+    content: 5,
+    size: 64,
+    price: 5
+  },
+  {
+    name: 'Parsec Pilsner',
+    content: 6.5,
+    size: 101,
+    price: 6.50
+  }
+];
 
 function Body() {
   return (
     <div className="bodyDiv">
-    
+    <hr/>
+    {masterKegList.map((keg, index) =>
+      <Keg name={keg.name}
+      content={keg.content}
+      size={keg.size}
+      price={keg.price}
+      key={index}/>
+    )}
+
     </div>
   );
 }
