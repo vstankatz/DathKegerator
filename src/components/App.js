dae629904b3cbe66dc0a8880cd85ee5e194ff91a
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-14 10:52:51 -0800

    Import Header, Body, AddKeg, and Edit Keg. Connect through Switch Routes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Body from './Body';
+import AddKeg from './AddKeg';
+import EditKeg from './EditKeg';
+import { Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+    <Header/>
+    <Switch>
+    <Route exact path='/' component={Body} />
+    <Route exact path='/addkeg' component={AddKeg} />
+    <Route exact path='/editkeg' component={EditKeg} />
+    </Switch>
 
     </div>
   );
