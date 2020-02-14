0fb27002000cbd4a8423942d02f87ccde9394e8c
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-14 15:02:23 -0800

    Add proptypes to Keg.

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Keg.css';
+import PropTypes from 'prop-types';
 
-function Keg() {
+function Keg(props) {
+  const amount = props.size;
+  const price = props.price;
   return (
     <div className="kegDiv">
-
+    <hr/>
+    <h4>{props.name}</h4>
+    <p>{props.price} <em>{props.content}</em></p>
+    <span>{props.size}</span>
     </div>
   );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string,
+  price: PropTypes.number,
+  content: PropTypes.number,
+  size: PropTypes.number
 }
 
 export default Keg;
