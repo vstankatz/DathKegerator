1054e0831505cf0b98d35c9e61a82a0e29bd9daf
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-14 16:16:07 -0800

    Attempts to get divs to change dylamically based on price, fails.

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import './Keg.css';
 import PropTypes from 'prop-types';
 
+
 function Keg(props) {
+
   const amount = props.size;
   const price = props.price;
-  return (
-    <div className="kegDiv">
+  if (price < 5) {
+    function low() {
+      var element = document.getElementById('kegDiv');
+      console.log(element);
+      element.classList.add("lowPrice");
+    }
+     low();
+  }
+    return (
+    <div className="kegDiv" id='kegDiv'>
     <hr/>
     <h4>{props.name}</h4>
     <p>{props.price} <em>{props.content}</em></p>
