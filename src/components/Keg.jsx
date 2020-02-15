8e03126a5407ad7cdb16870fad67208d91706c0b
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-14 16:41:22 -0800

    Add span tags and css.

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -4,23 +4,21 @@ import PropTypes from 'prop-types';
 
 
 function Keg(props) {
-
-  const amount = props.size;
-  const price = props.price;
-  if (price < 5) {
-    function low() {
-      var element = document.getElementById('kegDiv');
-      console.log(element);
-      element.classList.add("lowPrice");
-    }
-     low();
+  let drinkName = {
+    fontFamily: "'Sonsie One', cursive",
+    fontSize:'1.5vw'
   }
-    return (
-    <div className="kegDiv" id='kegDiv'>
+  let contentSpan = {
+    fontSize: '.8vw',
+    margin: '.4vw',
+    color: 'grey'
+  }
+  return (
+    <div className="kegDiv">
     <hr/>
-    <h4>{props.name}</h4>
-    <p>{props.price} <em>{props.content}</em></p>
-    <span>{props.size}</span>
+    <h4 style={drinkName}>{props.name}<span style={contentSpan}>-<em>{props.content}%</em></span></h4>
+    <p>${props.price}<span className='priceSpan'>/pint</span></p>
+    <span>{props.size} pints left</span>
     </div>
   );
 }
