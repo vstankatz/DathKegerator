aec9b35f49b012fb2e7be3d901b8a22861d5ae16
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-21 09:12:47 -0800

    Update Routes.

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,14 @@ import Header from './Header';
 import Body from './Body';
 import AddKeg from './AddKeg';
 import EditKeg from './EditKeg';
+import SiteController from './AgeCheck';
 import { Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    <Header/>
     <Switch>
-    <Route exact path='/' component={Body} />
+    <Route exact path='/' component={SiteController} />
     <Route exact path='/addkeg' component={AddKeg} />
     <Route exact path='/editkeg' component={EditKeg} />
     </Switch>
