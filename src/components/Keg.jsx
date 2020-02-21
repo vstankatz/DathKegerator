27344f40e45ae6dd5f5d6385f2669a176b26c7e3
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-21 14:14:48 -0800

    Add check for radio button choice.

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -13,12 +13,38 @@ function Keg(props) {
     margin: '.4vw',
     color: 'grey'
   }
+
+console.log(props);
+  let total = null;
+  function translateSize(size){
+    if (size === 'mini') {
+      console.log(size);
+      total = 10
+    } else if (size === 'litre') {
+      total = 105
+    } else if (size === 'half') {
+      total = 124
+    } else if (size === 'slim') {
+      total = 62
+    } else if (size === 'quarter') {
+      total = 62
+    } else if (size === 'sixth') {
+      total = 41
+    } else if (size === 'cornelius') {
+      total = 40
+    } else {
+      total = 0
+    }
+    return total
+  }
+
   return (
     <div onClick={() => {props.onSelectedKegCorrection({name: props.name, content: props.content, price: props.price, size: props.size, id: props.id})}} className="kegDiv">
     <hr/>
     <h4 style={drinkName}>{props.name}<span style={contentSpan}>-<em>{props.content}%</em></span></h4>
     <p>${props.price}<span className='priceSpan'>/pint</span></p>
-    <span>{props.size} pints left</span>
+    <span>{translateSize(props.size)} pints left</span>
+    <button onClick={() => props.onPouringGlass(translateSize(props.size))}>pour glass</button>
     </div>
   );
 }
@@ -28,7 +54,8 @@ Keg.propTypes = {
   price: PropTypes.string,
   content: PropTypes.string,
   size: PropTypes.string,
-  onSelectedKegCorrection: PropTypes.func
+  onSelectedKegCorrection: PropTypes.func,
+  onPouringGlass: PropTypes.func
 }
 
 export default Keg;
