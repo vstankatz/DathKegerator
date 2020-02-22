c304969f9d9fb0b33509e949098adac5cd5eeb4d
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-21 16:29:52 -0800

    Gives state to keg to try and add crud functionality.

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Keg.css';
 import PropTypes from 'prop-types';
 
 
-function Keg(props) {
-  let drinkName = {
-    fontFamily: "'Sonsie One', cursive",
-    fontSize:'1.5vw'
-  }
-  let contentSpan = {
-    fontSize: '.8vw',
-    margin: '.4vw',
-    color: 'grey'
+class Keg extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isUpdating: false,
+    }
+    this.editKeg = this.editKeg.bind(this);
+    this.updateKegDone = this.updateKegDone.bind(this);
+    this.deleteKeg = this.deleteKeg.bind(this)
   }
 
-console.log(props);
-  let total = null;
-  function translateSize(size){
+
+
+  editKeg(){
+    const {id} = this.props.selectedKeg
+    this.setState((prevState, props) => ({
+      isUpdating : !prevState.isUpdating
+    }))
+  }
+
+  updateKegDone(){
+    const {id} = this.props.selectedKeg
+    this.setState((prevState, props) => ({
+      isUpdating : !prevState.isUpdating
+    }))
+    this.props.updateKegDone(
+      id,
+      this.name.value,
+      this.content.value,
+      this.price.value,
+      this.size.value
+    )
+  }
+
+  deleteKeg(){
+    const {id} = this.props.selectedKeg
+
+  }
+
+  translateSize(size){
+    let total = null
     if (size === 'mini') {
       console.log(size);
       total = 10
@@ -38,17 +65,41 @@ console.log(props);
     return total
   }
 
-  return (
-    <div onClick={() => {props.onSelectedKegCorrection({name: props.name, content: props.content, price: props.price, size: props.size, id: props.id})}} className="kegDiv">
-    <hr/>
-    <h4 style={drinkName}>{props.name}<span style={contentSpan}>-<em>{props.content}%</em></span></h4>
-    <p>${props.price}<span className='priceSpan'>/pint</span></p>
-    <span>{translateSize(props.size)} pints left</span>
-    <button onClick={() => props.onPouringGlass(translateSize(props.size))}>pour glass</button>
-    </div>
-  );
-}
+  render(){
+    let total = null;
+    const {name, content, price, size, id} = this.props.onSelectedKegCorrection;
 
+    let drinkName = {
+      fontFamily: "'Sonsie One', cursive",
+      fontSize:'1.5vw'
+    }
+
+    let contentSpan = {
+      fontSize: '.8vw',
+      margin: '.4vw',
+      color: 'grey'
+    }
+
+  return (
+    this.state.isUpdating === true ? (
+      <p> updating </p>
+    ) : (
+
+    <div className="kegDiv">
+    <hr/>
+    <h4 style={drinkName}>{this.props.name}<span style={contentSpan}>-<em>{this.props.content}%</em></span></h4>
+    <p>${this.props.price}<span className='priceSpan'>/pint</span></p>
+    <span>{this.translateSize(this.props.size)} pints left</span>
+
+    <button onClick={() => this.props.onPouringGlass(this.translateSize(this.props.size))}>pour glass</button>
+
+    <button onClick={() => {this.props.onSelectedKegCorrection({name: this.props.name, content: this.props.content, price: this.props.price, size: this.props.size, id: this.props.id})}}>Edit</button>
+    </div>
+
+  )
+)
+}
+}
 Keg.propTypes = {
   name: PropTypes.string,
   price: PropTypes.string,
